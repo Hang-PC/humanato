f601f96877c7dab732f81d8c9971caab48bdfb09
Sreesanjay s <sreesanjay7592@gmail.com>
2024-12-08 10:49:36 +0530

    chore: tailwind config completed

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#388087",
+        ["primary-hover"]: "#2c787f",
+        "primary-bg": "#111827",
+        "seconday-bg": "#1f2937",
+        // "primary": "#cc5de8",
+        // ["primary-hover"]: "#C25DD2",
+        secondary: "#FFFFFF",
+        tertiary: "#E2E3F4",
+      },
+    },
+  },
+  plugins: [],
+};
