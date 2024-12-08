e6c09ec662e31c58df8f7886a56b5e0d2c074dcc
shuhaib-t <shuhaibtu79@gmail.com>
2024-12-08 11:45:58 +0530

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
-    <main className="h-screen bg-primary">
+    <main className="h-screen ">
+      <Header />
       <Hero />
     </main>
   );
