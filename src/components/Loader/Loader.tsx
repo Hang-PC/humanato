d45846f11b69a2d9537884d0abc073aeb65d1590
shuhaib-t <shuhaibtu79@gmail.com>
2024-12-08 12:51:09 +0530

    refactor: update navbar style

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const Loader = () => {
+  return (
+    <div className="w-full h-screen bg-primary flex items-center justify-center">
+      <div className="loader"></div>;
+    </div>
+  );
+};
+
+export default Loader;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { lazy, StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Loader from "./components/Loader/Loader.tsx";
+const App = lazy(() => import("./App.tsx"));
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Suspense fallback={<Loader />}>
+      <App />
+    </Suspense>
+  </StrictMode>
+);
